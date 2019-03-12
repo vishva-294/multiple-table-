d0b4c7e59a872db659deb37dd062f670e7d7ea26
vishva-294 <48483587+vishva-294@users.noreply.github.com>
2019-03-12 07:23:16 -0700

    Add files via upload

---
+++ multiTable.java
@@ -0,0 +1,10 @@
+multipleTable(1)
+multipleTable(9)
+int i,j;
+int i,j;
+void multipleTable(int i){
+   for(j=1;j<=10;j++)
+      { System.out.printf("%d*%d=%d",i,j,i*j).println();
+      }
+    }
+multipleTable(2)
